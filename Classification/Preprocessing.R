43cac4c885730ecd26bea2ca676ce85ff6c63434
cariahmarey <marius.behret@gmail.com>
2023-08-24 19:44:59 +0200

    update

--- Classification/Preprocessing.R
+++ Classification/Preprocessing.R
@@ -7,7 +7,7 @@ allprofiles_df <- read.csv("EPINetz_TwitterPoliticians_2021.csv")
 # rename column "user" to "twitter_handle"
 colnames(profiledescriptions_df)[2] <- "twitter_handle"
 
-# get userids from allprofiles_df
+# get userids and party from allprofiles_df
 profiledescriptions_df$user_id <- allprofiles_df$user_id[match(profiledescriptions_df$twitter_handle, allprofiles_df$twitter_handle)]
 profiledescriptions_df$party <- allprofiles_df$party[match(profiledescriptions_df$twitter_handle, allprofiles_df$twitter_handle)]
 
@@ -47,35 +47,7 @@ labeled_profiles_df <- data.frame(lapply(labeled_profiles_df, function(x) {
   }
 }))
 
-# modify dataframe for BERT
-library(data.table)
-labeled_profiles_df_bert <- labeled_profiles_df
-
-# Add a new column with value 1 to the dataframe
-labeled_profiles_df_bert$value <- 1
-
-# Convert df to a data table
-setDT(labeled_profiles_df_bert)
-
-# Reshape data
-labeled_profiles_df_bert_wide <- dcast(labeled_profiles_df_bert, ... ~ label, value.var = "value", fun.aggregate = sum, fill = 0)
-
-# Now, remove the original 'label' column if it exists in df_wide
-labeled_profiles_df_bert_wide[, label := NULL]
-
-#export csv for BERT
-write.table(labeled_profiles_df_bert_wide, 
-            file = "initial_train_bert.csv",
-            sep = ",",          # Use tab as the delimiter
-            eol = "\n",          # Set end of line character
-            na = "",             # How to represent missing values
-            row.names = FALSE,   # Do not write row names
-            col.names = TRUE,    # Write column names
-            quote = TRUE,        # Use quotes
-            qmethod = "double")  # Double quotes for escaping quotes
-
-
-# modify dataframe & export csv for small-text
+#-- modify dataframe & export csv for small-text
 labeled_profiles_df_smalltext <- labeled_profiles_df[c("example", "label")]
 
 write.table(labeled_profiles_df_smalltext, 
@@ -89,6 +61,64 @@ write.table(labeled_profiles_df_smalltext,
             qmethod = "double")  # Double quotes for escaping quotes
 
 
+
+
+
+#-- modify dataframe for BERT
+library(readxl)
+library(dplyr)
+
+# import xlsx file with the examples labeled in the small-text classification process
+labeled_profiles_df
+
+file_path <- "True Labels/CER_ClassificationProcess.xlsx"
+
+# List all the sheets in the Excel file
+sheet_names <- excel_sheets(file_path)
+
+# Initialize an empty data frame
+classified_examples_fromsmalltext <- data.frame()
+
+# Loop through the sheet names, read each sheet, and append to the all_data dataframe
+for (sheet in sheet_names) {
+  sheet_data <- read_excel(file_path, sheet = sheet)
+  classified_examples_fromsmalltext <- bind_rows(classified_examples_fromsmalltext, sheet_data)
+}
+
+# rename column "text" to "example"
+colnames(classified_examples_fromsmalltext)[1] <- "example"
+# rename column "true label" to "label"
+colnames(classified_examples_fromsmalltext)[3] <- "label"
+# delete second column with predicted label
+classified_examples_fromsmalltext[[2]] <- NULL
+
+# rename column "text" to "example" from full dataset
+colnames(labeledandunlabeled_profiles_df)[3] <- "example"
+# rename column "true label" to "label" from full dataset
+colnames(labeledandunlabeled_profiles_df)[6] <- "label"
+
+# get X, twitter_handle, user_id and party from full dataset
+classified_examples_fromsmalltext$X <- labeledandunlabeled_profiles_df$X[match(classified_examples_fromsmalltext$example, labeledandunlabeled_profiles_df$example)]
+classified_examples_fromsmalltext$twitter_handle <- labeledandunlabeled_profiles_df$twitter_handle[match(classified_examples_fromsmalltext$example, labeledandunlabeled_profiles_df$example)]
+classified_examples_fromsmalltext$user_id <- labeledandunlabeled_profiles_df$user_id[match(classified_examples_fromsmalltext$example, labeledandunlabeled_profiles_df$example)]
+classified_examples_fromsmalltext$party <- labeledandunlabeled_profiles_df$party[match(classified_examples_fromsmalltext$example, labeledandunlabeled_profiles_df$example)]
+
+labeled_profiles_df$label <- as.character(labeled_profiles_df$label)
+classified_examples_fromsmalltext$label <- as.character(classified_examples_fromsmalltext$label)
+
+
+labeled_profiles_df_bert <- bind_rows(labeled_profiles_df, classified_examples_fromsmalltext)
+
+#export csv for BERT
+write.table(labeled_profiles_df_bert, 
+            file = "initial_train_bert.csv",
+            sep = ",",          # Use tab as the delimiter
+            eol = "\n",          # Set end of line character
+            na = "",             # How to represent missing values
+            row.names = FALSE,   # Do not write row names
+            col.names = TRUE,    # Write column names
+            quote = TRUE,        # Use quotes
+            qmethod = "double")  # Double quotes for escaping quotes
 
 #-------- create dataframe with only unlabeled rows
 library(xlsx)
